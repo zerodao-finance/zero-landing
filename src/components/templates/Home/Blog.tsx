c529062e154af5c9960177cd65ed611975e7de8d
yoyobojo <angellolazar@gmail.com>
2022-07-10 11:35:29 -0400

    added loading spinner for blog posts

--- src/components/templates/Home/Blog.tsx
+++ src/components/templates/Home/Blog.tsx
@@ -3,19 +3,22 @@ import { Background } from '../../background/Background';
 import { BlogPreview } from '../../cards/BlogPreview';
 import { Grid } from '../../layout/Grid';
 import { Section } from '../../layout/Section';
+import { Spinner } from '../../loaders/Spinner';
 import { SectionTitle } from '../../typography';
 
 function Blog() {
-  const { blogData, blogError } = useBlogs();
+  const { blogData, blogLoading, blogError } = useBlogs();
 
   return (
     <Background color="bg-gray-900">
-      <Section vertical verticalCenter yPadding="py-10">
+      <Section vertical verticalCenter yPadding="py-20">
         <SectionTitle text="Blog" />
         {blogError ? (
           <p className="text-center">
             Error retrieving blog data. Please try again later.
           </p>
+        ) : blogLoading ? (
+          <Spinner padding="py-20" />
         ) : (
           <Grid>
             {blogData.map((el, i) => (
