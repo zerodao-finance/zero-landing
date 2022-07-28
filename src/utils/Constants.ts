3790df28982f72bda1224bbb45ef872a00c140f0
yoyobojo <angellolazar@gmail.com>
2022-07-28 12:10:31 -0400

    deleted redundant coin svg components

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,14 +1,5 @@
 import { ethers } from 'ethers';
 
-import AVAXSvg from './svg/coins/avax';
-import BTCSvg from './svg/coins/btc';
-import ETHSvg from './svg/coins/eth';
-import ibBTCSvg from './svg/coins/ibbtc';
-import MATICSvg from './svg/coins/matic';
-import renBTCSvg from './svg/coins/renbtc';
-import USDCSvg from './svg/coins/usdc';
-import WBTCSvg from './svg/coins/wbtc';
-
 // Styles
 export const hoverWhite = 'transition duration-200 hover:text-gray-100';
 
@@ -37,41 +28,33 @@ export const tokens = [
   {
     src: '/assets/svg-coins/btc.svg',
     alt: 'BTC',
-    svg: BTCSvg,
   },
   {
     src: '/assets/svg-coins/eth.svg',
     alt: 'ETH',
-    svg: ETHSvg,
   },
   {
     src: '/assets/svg-coins/usdc.svg',
     alt: 'USDC',
-    svg: USDCSvg,
   },
   {
     src: '/assets/svg-coins/renbtc.svg',
     alt: 'renBTC',
-    svg: renBTCSvg,
   },
   {
     src: '/assets/svg-coins/wbtc.svg',
     alt: 'WBTC',
-    svg: WBTCSvg,
   },
   {
     src: '/assets/svg-coins/ibbtc.svg',
     alt: 'ibBTC',
-    svg: ibBTCSvg,
   },
   {
     src: '/assets/svg-coins/avax.svg',
     alt: 'AVAX',
-    svg: AVAXSvg,
   },
   {
     src: '/assets/svg-coins/matic.svg',
     alt: 'MATIC',
-    svg: MATICSvg,
   },
 ];
