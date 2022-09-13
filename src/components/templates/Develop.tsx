7ac20923655ee133d895a83b5bcba84ed58a1020
yoyobojo <angellolazar@gmail.com>
2022-09-13 17:21:33 -0400

    added template to page

---
+++ src/components/templates/Develop.tsx
@@ -0,0 +1,15 @@
+// Layout
+import { Section } from '../layout/Section';
+
+// Typography
+import { SectionTitle } from '../typography';
+
+const Develop = () => {
+  return (
+    <Section vertical verticalCenter yPadding="pb-20 pt-28">
+      <SectionTitle text="Developer Docs" />
+    </Section>
+  );
+};
+
+export { Develop };
---
+++ src/pages/develop.tsx
@@ -0,0 +1,12 @@
+import { Base } from '../components/templates/Base';
+import { Develop } from '../components/templates/Develop';
+
+const DevelopPage = () => {
+  return (
+    <Base withNav>
+      <Develop />
+    </Base>
+  );
+};
+
+export default DevelopPage;
